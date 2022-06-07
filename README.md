1a55d4468c696acce98045e411450e99179f00dd
Anonyvius <30316592+Anonyvius@users.noreply.github.com>
2022-06-07 19:51:24 +0200

    initial

---
+++ README.md
@@ -0,0 +1,63 @@
+# emkei.cz
+
+emkei.cz is a spammer / WIP API that uses the website [emkei.cz](https://emkei.cz/) to send emails from any address, to any address. It uses the 2captcha API to bypass the hCaptcha proposed by the website
+
+## Prerequisites
+
+- have python3+ installed.
+- 2captcha API key with balance
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install the packages requests and twocaptcha.
+
+```bash
+> pip3 install requests
+> pip3 install twocaptcha 
+```
+
+## Installation
+
+Use `git clone` to download the repository to your local machine
+
+```bash
+> git clone https://github.com/Anonyvius/emkei.cz
+```
+
+## Usage
+
+Open the config.json and edit the variables to your preferences.
+
+```json
+{
+    "sender_name": "Google Admin",
+    "sender_email": "admin@gmail.com",
+    "receiver_email": "template_email@gmail.com",
+    "subject": "Test email",
+    "message": "this is a test",
+    "api_key": "YOUR 2CAPTCHA API KEY",
+    "balance_stop": 1.0
+}
+```
+
+`threads` defines the amout of concurrent threads that will be used to send emails.
+`apiKey` is your 2captcha API key.
+`balance_stop` automatically stops the program if your 2captcha balance dips below a certain number.
+Everything else should be self explanatory
+
+Execute the auto.py
+
+```bash
+> python3 auto.py
+```
+
+![output](https://i.imgur.com/kAa6gAY.png)
+![received email](https://i.imgur.com/tZhqZOc.png)
+
+(the emails are very likely to go into the spam folder, there is currently no fix for this, that I am aware of)
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+# Disclaimer
+This repository is for research purposes only, the use of this code is your responsibility.
+
+I take NO responsibility and/or liability for how you choose to use any of the source code available here. By using any of the files available in this repository, you understand that you are AGREEING TO USE AT YOUR OWN RISK. Once again, ALL files available here are for EDUCATION and/or RESEARCH purposes ONLY.
---
+++ auto.py
@@ -0,0 +1,83 @@
+import requests, threading, json
+from twocaptcha import TwoCaptcha
+
+config = json.loads(open('config.json', 'r').read())
+
+captcha_config = {
+            'apiKey': config['apiKey'],
+            'defaultTimeout': 200,
+            'recaptchaTimeout': 600,
+        }
+
+solver = TwoCaptcha(**captcha_config)
+
+
+headers = {
+    'Host': 'emkei.cz',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Origin': 'null'
+}
+
+def checkBalance():
+    try:
+        balance = solver.balance()
+    except Exception as e:
+        print(e)
+        quit()
+    if balance < config['balance_stop']:
+        print('Not enough credits!')
+        return True
+    else:
+        return False
+
+def sender():
+    global counter
+    counter = 0
+    while True:
+        if checkBalance():
+            break
+        if requests.get('http://emkei.cz/').status_code != 200:
+            print('IP blocked!')
+            break
+
+        try:
+            captcha_response = solver.hcaptcha(sitekey='3fbaf8b1-0fee-45c1-95ec-39b2f57a4fba', url='https://emkei.cz/?hCaptcha')
+        except:
+            continue
+
+        data = {
+            'fromname': config['sender_name'],
+            'from': config['sender_email'],
+            'rcpt': config['receiver_email'],
+            'subject': config['subject'],
+            'attachment[]': '(binary)',
+            'importance': 'normal',
+            'xmailer': '0',
+            'current': 'on',
+            'charset': 'utf-8',
+            'encrypt': 'no',
+            'ctype': 'plain',
+            'rte': '0',
+            'text': config['message'],
+            'g-recaptcha-response': captcha_response['code'],
+            'h-captcha-response': captcha_response['code'],
+            'ok': 'Send'
+        }
+        
+        r = requests.post('https://emkei.cz/?hCaptcha', data=data, headers=headers)
+        if 'E-mail sent successfully' in r.text:
+            counter += 1
+            print(f'{counter} emails send!')
+            solver.report(captcha_response['captchaId'], True)
+        elif "The hCaptcha test wasn't successful. Please try again." in r.text:
+            print('Captcha failed!')
+            solver.report(captcha_response['captchaId'], False)
+        else:
+            print('Unknown error!')
+            out = open('error.html', 'w').write(r.text)
+
+for i in range(config['threads']):
+    t = threading.Thread(target=sender)
+    t.start()
